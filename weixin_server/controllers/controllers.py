8a1ee3e366184fb1dd7f47d48570d85deabacd2e
Abner <2906631584@qq.com>
2017-12-25 02:04:45 +0800

    测试微信服务的交互操作!!!22

--- weixin_server/controllers/controllers.py
+++ weixin_server/controllers/controllers.py
@@ -59,7 +59,7 @@ class WeixinServer(http.Controller):
                 return echostr
             else:
                 print u'走的是这条路吗!!!'
-                return response
+                return etree.tostring(parsed)
 
         else:
             return 'error'
