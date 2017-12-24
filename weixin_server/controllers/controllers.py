5758ce5e9096b5614c74755e35a69e3940a7792d
Abner <2906631584@qq.com>
2017-12-24 22:44:21 +0800

    测试微信请求的数据是否传递过来!!!

--- weixin_server/controllers/controllers.py
+++ weixin_server/controllers/controllers.py
@@ -1,7 +1,23 @@
 # -*- coding: utf-8 -*-
 from odoo import http
+from odoo.http import  request
+import hashlib
 
-# class WeixinServer(http.Controller):
+class WeixinServer(http.Controller):
+
+
+    def check_signature(signature, timestamp, nonce, token):
+        L = [timestamp, nonce, token]
+        L.sort()
+        s = L[0] + L[1] + L[2]
+        return hashlib.sha1(s).hexdigest() == signature
+
+    @http.route('/weixin_server/', auth='public')
+    def echo(self, signature, timestamp, nonce, token):
+        print request
+        print signature
+        return 'ok'
+
 #     @http.route('/weixin_server/weixin_server/', auth='public')
 #     def index(self, **kw):
 #         return "Hello, world"
